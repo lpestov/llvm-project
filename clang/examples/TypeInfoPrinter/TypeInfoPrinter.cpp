9f7bed32f4c42d63705397b2ecfea3a9908690aa
leo <Pestov.962@gmail.com>
2026-02-18 21:31:50 +0300

    delete trash

--- clang/examples/TypeInfoPrinter/TypeInfoPrinter.cpp
+++ clang/examples/TypeInfoPrinter/TypeInfoPrinter.cpp
@@ -1,11 +1,3 @@
-//===- TypeInfoPrinter.cpp ------------------------------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 #include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
